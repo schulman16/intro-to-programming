789dd42fd756a378241ec801675c5dbfa509ca8d
Daniel Schulman <Dan.Schulman@gmail.com>
2017-10-28 17:36:00 -0700

    Variables exercises

---
+++ name.rb
@@ -0,0 +1,22 @@
+puts "Hello, please enter your name: "
+name = gets.chomp
+puts "Welcome, #{name}! It's very nice to meet you!"
+
+#Exercise 3 - printing name 10 names
+puts
+puts
+
+10.times do 
+  puts name
+  end
+
+puts
+puts
+#Exercise 4
+
+puts "What's your first name? "
+f_name = gets.chomp
+puts "What's your last name? "
+l_name = gets.chomp
+
+puts "Nice to meet you #{f_name} #{l_name}"
